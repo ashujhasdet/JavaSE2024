dcddd379be1172b922738de73203451dcb846ebc
Aashutosh Jha <145312210+ashujhasdet@users.noreply.github.com>
2024-03-14 11:47:56 +0530

    Add files via upload

---
+++ programmingSet2/AsciiBigAlphabet.java
@@ -0,0 +1,41 @@
+package programmingSet2;
+// A Java program to print ASCII value of 'A' to 'Z'
+public class AsciiBigAlphabet {
+	public static void main (String[] args) {
+		System.out.println("Main Start");
+		System.out.println("ASCII VALUES OF A TO Z: ");
+		System.out.println("-------------------------");
+		//typecast from char to int 
+		//TRICK : Let position of A = 0 , B =1 ..etc. 
+		//To Find ASCII VALUE FROM A TO Z USE THIS
+		// POSITION VALUE + 65
+		System.out.println("ASCII VALUE OF A = "+ (int)'A');//0 +65
+		System.out.println("ASCII VALUE OF B = "+ (int)'B');//1+65=66
+		System.out.println("ASCII VALUE OF C = "+ (int)'C');
+		System.out.println("ASCII VALUE OF D = "+ (int)'D');
+		System.out.println("ASCII VALUE OF E = "+(int)'E');
+		System.out.println("ASCII VALUE OF F = " +(int)'F');
+		System.out.println("ASCII VALUE OF G = "+ (int)'G');
+		System.out.println("ASCII VALUE OF H = "+(int)'H');
+		System.out.println("ASCII VALUE OF I = "+(int)'I');
+		System.out.println("ASCII VALUE OF J = "+(int)'J');
+		System.out.println("ASCII VALUE OF K = "+(int)'K');
+		System.out.println("ASCII VALUE OF L = "+(int)'L');
+		System.out.println("ASCII VALUE OF M = "+(int)'M');
+		System.out.println("ASCII VALUE OF N = "+(int)'N');
+		System.out.println("ASCII VALUE OF O = "+(int)'O');
+		System.out.println("ASCII VALUE OF P = "+(int)'P');
+		System.out.println("ASCII VALUE OF Q = "+(int)'Q');
+		System.out.println("ASCII VALUE OF R = "+(int)'R');
+		System.out.println("ASCII VALUE OF S = "+(int)'S');
+		System.out.println("ASCII VALUE OF T = "+(int)'T');
+		System.out.println("ASCII VALUE OF U = "+(int)'U');
+		System.out.println("ASCII VALUE OF V = "+(int)'V');
+		System.out.println("ASCII VALUE OF W = "+(int)'W');
+		System.out.println("ASCII VALUE OF X = "+(int)'X');
+		System.out.println("ASCII VALUE OF Y = "+(int)'Y');
+		System.out.println("ASCII VALUE OF Z = "+(int)'Z');
+		System.out.println("Main Ends");
+	}
+
+}
---
+++ programmingSet2/AsciiSmallAlphabets.java
@@ -0,0 +1,37 @@
+package programmingSet2;
+//A Java program to print ASCII value from 'a' to 'z'
+public class AsciiSmallAlphabets {
+	public static void main(String [] args) {
+		System.out.println("Main Start");
+		//We have charecters 'a','b'... we need to type cast to int value for getting ASCII value
+		System.out.println("ASCII VALUE OF a TO z :");
+		System.out.println("ASCII Value of a = "+(int)'a');//Type casted char To int
+		System.out.println("ASCII Value of b = "+(int)'b');
+		System.out.println("ASCII Value of c = "+(int)'c');
+		System.out.println("ASCII Value of d = "+(int)'d');
+		System.out.println("ASCII Value of e = "+(int)'e');
+		System.out.println("ASCII Value of f = "+(int)'f');
+		System.out.println("ASCII Value of g = "+(int)'g');
+		System.out.println("ASCII Value of h = "+(int)'h');
+		System.out.println("ASCII Value of i = "+(int)'i');
+		System.out.println("ASCII Value of j = "+(int)'j');
+		System.out.println("ASCII Value of k = "+(int)'k');
+		System.out.println("ASCII Value of l = "+(int)'l');
+		System.out.println("ASCII Value of m = "+(int)'m');
+		System.out.println("ASCII Value of n = "+(int)'n');
+		System.out.println("ASCII Value of o = "+(int)'o');
+		System.out.println("ASCII Value of p = "+(int)'p');
+		System.out.println("ASCII Value of q = "+(int)'q');
+		System.out.println("ASCII Value of r = "+(int)'r');
+		System.out.println("ASCII Value of s = "+(int)'s');
+		System.out.println("ASCII Value of t = "+(int)'t');
+		System.out.println("ASCII Value of u = "+(int)'u');
+		System.out.println("ASCII Value of v = "+(int)'v');
+		System.out.println("ASCII Value of w = "+(int)'w');
+		System.out.println("ASCII Value of x = "+(int)'x');
+		System.out.println("ASCII Value of y = "+(int)'y');
+		System.out.println("ASCII Value of z = "+(int)'z');
+		System.out.println("Main Ends");
+	}
+
+}
